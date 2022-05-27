a9e0e9b4ef218a593f6518dc513d68476fc995b8
YuriShang <yuri.shangaraev@gmail.com>
2022-05-27 20:55:31 +0500

    задание выполнено

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 from fast_bitrix24 import Bitrix
+from datetime import date, timedelta
+import requests 
 
 
-webhook = 'https://b24-ddwojh.bitrix24.ru/rest/1/qo5s8qed3wmy7gx6/'
-b = Bitrix(webhook)
+def main():
+    # сегодняшний день + 3 дня
+    day = ''.join(str(date.today() + timedelta(days=3)).split('-')) 
+    webhook = 'здесь должен быть ваш вебхук'
 
-add_task = b.call('tasks.task.add', {
-   "taskId":"1",
-   "fields": {
-       "TITLE": "Тестовая задача",
-       "DESCRIPTION": "Тестовое описание",
-       "PRIORITY": 2,
-       "RESPONSIBLE_ID": 1,
-   }
-})
+    # создаем экземпляр класса Bitrix и передаем в него свой вебхук
+    my_bitrix = Bitrix(webhook)
+
+    # с помощью гет запроса проверяем является ли переданный день праздничным:
+    request =requests.get(f'https://isdayoff.ru/{day}?holiday=1') 
+
+    # если ответ == '8', то день праздничный. Инфа с оф. телеграма сервиса, работает только для России.
+    # если условие выполняется, создаем задачку в б24
+    if request.text == '8':
+        my_bitrix.call('tasks.task.add', {
+            "taskId":"1",
+            "fields": {
+                "TITLE": "Тестовая задача",
+                "DESCRIPTION": "Тестовое описание",
+                "PRIORITY": 2,
+                "RESPONSIBLE_ID": 1,
+            }
+        })
+
+
+if __name__ == "__main__":
+    main()
